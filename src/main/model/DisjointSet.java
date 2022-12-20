6a5a248b24231f984c4c9385dfbbacd741da359c
arulh <arul.howard@telus.net>
2022-12-20 11:50:31 -0800

    Initial commit

---
+++ src/main/model/DisjointSet.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.ArrayList;
+
+public class DisjointSet {
+    ArrayList<Integer> sets;
+
+    public DisjointSet(int size) {
+        sets = new ArrayList<>(size);
+    }
+
+    private void find(int n) {
+
+    }
+
+    private void union(int a, int b) {
+
+    }
+}
